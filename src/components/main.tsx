15b18b72668a8b343229d2163aba04811d517fa4
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-07-17 22:20:53 +0200

    fix mui styles

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,17 +1,15 @@
-import '../assets/css/styles.css'
-// @ts-ignore
-import React from 'react'
-// @ts-ignore
-import ReactDOM from 'react-dom/client'
-
-import App from './App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
 import {store} from "../redux/store";
-import {Provider} from "react-redux";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.render(
+    <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
-)
+    </React.StrictMode>,
+    document.getElementById('root')
+);
