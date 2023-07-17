df6c972b0e8753adc6e816f81b5173ab1d27181a
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-07-17 21:34:56 +0200

    fix mui styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly'
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/variables.scss";`
-      }
-    }
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc',
     },
-  },
+  }
 })
