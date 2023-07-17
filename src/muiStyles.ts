2b76d26e9bf4d734e15dc159d77f32f6069a0f4e
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-07-17 18:31:59 +0200

    updating sidebar ui

--- src/muiStyles.ts
+++ src/muiStyles.ts
@@ -10,7 +10,7 @@ const textFieldStyles = {
             styleOverrides: {
                 root: {
                    ' &:hover': {
-                        backgroundColor: '#ADD0C0',
+                        backgroundColor: '#ADD0C0 !important',
                     },
                 },
             },
