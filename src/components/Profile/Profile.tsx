b28a97f2e61c3b83b14768ac1eeb42949d40ffd9
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-07-23 18:01:14 +0200

    updated small devices styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,85 +1,119 @@
 import '../../assets/css/profile.css'
-import {Checkbox, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
-import {Box} from "@mui/system";
+import {Avatar, Button, Checkbox, FormControl, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material";
+import {Stack} from "@mui/system";
 
 
 
 export default function Profile () {
   
+
   
   
   return (
     <div className="containerProfile">
       <section className="profile__banner">
-      <h2 className={"section-title"}>Your Profile</h2>
-      </section>
-    
-      <section className="profile__credentials">
-        <Box
-            sx={{
-              display:"flex",
-              flexDirection: "column",
-              height: "100%",
-              gap: "10px",
-              width: "40%",
-              boxSizing: "border-box",
-              paddingBottom: "20px",
-              margin:'auto',
-            }}
-        >
-          <TextField
-              label="Name"
-              variant="filled"
-              fullWidth
-          />
-          <TextField
-              label="LastName"
-              variant="filled"
-              fullWidth
-          />
-          <TextField
-              label="Email"
-              variant="filled"
-              fullWidth
-          />
-          <TextField
-              label="Phone Number"
-              variant="filled"
-              fullWidth
-          />
-          <FormControl fullWidth>
-            <InputLabel>Role</InputLabel>
-            <Select
-                variant="filled"
-            >
-              <MenuItem value={10}>Role 1</MenuItem>
-              <MenuItem value={20}>Role 2</MenuItem>
-              <MenuItem value={30}>Role 3</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel>Service</InputLabel>
-            <Select
-                variant="filled"
-            >
-              <MenuItem value={10}>Service 1</MenuItem>
-              <MenuItem value={20}>Service 2</MenuItem>
-              <MenuItem value={30}>Service 3</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel>Status</InputLabel>
-            <Select
-                variant="filled"
-            >
-              <MenuItem value={10}>Status 1</MenuItem>
-              <MenuItem value={20}>Status 2</MenuItem>
-              <MenuItem value={30}>Status 3</MenuItem>
-            </Select>
-          </FormControl>
+      <Avatar 
+        src="/broken-image.jpg"
+        sx={{
+          width: '90px',
+          height: '90px',
+          alignSelf: 'center',
+        }}
+      />
+      <Typography
+          variant="h5"
+          fontFamily={"var(--font-primary)"}
+          ml={4}
+          alignSelf='center'
+          letterSpacing={2}
+          sx={{
+            wordSpacing: '10px',
+          }}
 
-        </Box>
+      >Firstname Lastname</Typography>
+      <Stack 
+        className="profile__banner-info"
+        justifySelf={'flex-end'}
+        ml={'auto'}
+        p={2}
+        spacing={1}
+      >
+        <Typography fontFamily={"var(--font-primary)"} fontSize={'12px'} >Status : status </Typography>
+        <Typography fontFamily={"var(--font-primary)"} fontSize={'12px'} >Access : access </Typography>
+      </Stack>
       </section>
+      
+      <div className="profile__bottom-container">
+        <section className="profile__credentials">
+          <Stack spacing={1} alignSelf={'end'}
+              sx={{
+                flexDirection: "column",
+                height: "100%",
+                width: "100%",
+                justifyContent: "center",
+              }}
+          >
+            <TextField
+                label="Name"
+                variant="filled"
+                fullWidth
+            />
+            <TextField
+                label="LastName"
+                variant="filled"
+                fullWidth
+            />
+            <TextField
+                label="Email"
+                variant="filled"
+                fullWidth
+            />
+            <TextField
+                label="Phone Number"
+                variant="filled"
+                fullWidth
+            />
+            <FormControl fullWidth>
+              <InputLabel>Role</InputLabel>
+              <Select
+                  variant="filled"
+              >
+                <MenuItem value={10}>Role 1</MenuItem>
+                <MenuItem value={20}>Role 2</MenuItem>
+                <MenuItem value={30}>Role 3</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel>Service</InputLabel>
+              <Select
+                  variant="filled"
+              >
+                <MenuItem value={10}>Service 1</MenuItem>
+                <MenuItem value={20}>Service 2</MenuItem>
+                <MenuItem value={30}>Service 3</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel>Status</InputLabel>
+              <Select
+                  variant="filled"
+              >
+                <MenuItem value={10}>Status 1</MenuItem>
+                <MenuItem value={20}>Status 2</MenuItem>
+                <MenuItem value={30}>Status 3</MenuItem>
+              </Select>
+            </FormControl>
+              <Stack direction="row" spacing={1} alignItems="center" width={'100%'}>
+                <Button variant="contained" fullWidth>Save</Button>
+                <Button variant="contained" fullWidth>Edit</Button>
+                <Button variant="contained" fullWidth>Cancel</Button>
+              </Stack>
+          </Stack>
+        </section>
+        <section className="profile__options">
+
+        </section>
+      </div>
     </div>
     
   )
